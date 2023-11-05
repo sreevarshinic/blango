c6ae16f16c346eab86dde6c28e82cbe9e5bc3bd3
Sree C <sreevarshinic@gmail.com>
2023-11-05 16:40:00 +0000

    Finish html frameworks

--- blango/urls.py
+++ blango/urls.py
@@ -19,3 +19,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+# other imports
+import blog.views
+
+urlpatterns = [
+    # other patterns
+    path("", blog.views.index)
+]
